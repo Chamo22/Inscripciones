dbc1f5af2f775151f9e4d3008dba9f35ac69d477
Fabian Bobadilla <fabianaguirre147@gmail.com>
2024-05-20 14:20:39 -0300

    Inicio del proyecto

---
+++ Models/InscripcionesContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Inscripcioness.Models
+{
+    public class InscripcionesContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=./SQLEXPRESS;
+                           Database=InscripcionesContext;
+                           User Id = sa; Password = 123;
+                           MultipleActiveResultsSets = True;
+                           Encrypt=false");
+        }
+
+        public virtual DbSet<Alumno> alumnos { get; set; }
+    }
+}
